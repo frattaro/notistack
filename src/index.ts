bdaf1fc664bfc94c1ebacf8a5f02b0ae3e17780b
Anton Frattaroli <frattaro@gmail.com>
2024-09-01 20:43:16 -0400

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { SnackbarProvider, useSnackbar } from "./SnackbarProvider";
 export { SnackbarContent } from "./SnackbarContent";
+export type { SnackbarKey, SnackbarMessage, OptionsObject } from "./types";
